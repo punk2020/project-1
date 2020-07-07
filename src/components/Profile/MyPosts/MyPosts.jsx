ea6ac8f931c7fe67dc43d40d27cb4ff0d0321088
Clown2281337 <anton.gg.2006@gmai.com>
2020-07-07 21:56:06 +0300

    react createRef and button add post alerting text from text area

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,13 +4,25 @@ import Post from './Post/Post';
 
 function MyPosts(props) {
 
+  let NewPost = React.createRef();
+
+    let addpost = () => {
+      let text = NewPost.current.value;
+      alert(text);
+      //отсылка на Cyberpunk2077
+    }
 
 
-  let postsElements = props.posts.map(post => <Post message = {post.message} id = {post.id} likesCount = {post.likesCount}/>)
+  let postsElements = props.posts.map(post => <Post message={post.message} id={post.id} likesCount={post.likesCount} />)
 
   return (
-    <div className={c.content}>
+    <div>
       <div>My Posts</div>
+      <div classname={c.addpost}>
+        <textarea ref  = {NewPost}>Your post</textarea>
+        <div></div>
+        <button onClick={addpost}>Add post</button>
+      </div>
       <div>
         {postsElements}
       </div>
